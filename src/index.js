4535e76a7fe75ecabaaacf76ee211ee1283319b7
invalidtoken <codeit.sahil98@gmail.com>
2020-05-20 03:38:11 +0530

    use fetch hook

--- src/index.js
+++ src/index.js
@@ -1,16 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import CharacterList from './CharacterList';
-
+import endpoint from './endpoint';
 import dummyData from './dummy-data';
 
 import './styles.scss';
 
+const useFetch = (url) => {
+  console.log(2);
+  const [loading, setLoading] = useState('true');
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(undefined);
+
+  const setToLoading = () => {
+    setLoading(true);
+    setError(null);
+    setData(undefined);
+  };
+
+  const setToError = (error) => {
+    setLoading(false);
+    setError(error);
+    setData(undefined);
+  };
+
+  const setToData = (data) => {
+    setLoading(false);
+    setError(data);
+    setData(undefined);
+  };
+
+  useEffect(() => {
+    console.log(3);
+    setToLoading();
+
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(res.statusText);
+        }
+      })
+      .then((data) => {
+        if (data && data.characters) {
+          setToData(data);
+        }
+      })
+      .catch((error) => {
+        setToError(error);
+      });
+  }, [url]);
+
+  return { loading, error, data };
+};
+
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  console.log(1);
+  let { loading, error, data } = useFetch(endpoint + '/characters');
+  console.log(4);
+
+  console.log(loading, error, data);
 
   return (
     <div className="Application">
@@ -19,7 +72,7 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          <CharacterList characters={[]} />
         </section>
       </main>
     </div>
